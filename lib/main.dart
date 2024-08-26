06c68590428e111c34a85490e716bae2b73de182
Mahedy <mahedympc3@gmail.com>
2024-08-26 22:01:41 +0600

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
-  runApp(MyApp())
+  runApp(MyApp());
 }
 
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      ,
+
+
+
+
+    );
+  }
+}
 
 
 
